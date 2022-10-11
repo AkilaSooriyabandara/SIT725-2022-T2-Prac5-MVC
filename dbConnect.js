9a5b56a1817bb9b1100778178ff27d1d16059221
Akila Sooriyabandara <109722820+AkilaSooriyabandara@users.noreply.github.com>
2022-10-11 17:33:58 +1100

    updates -updated all the files.

---
+++ dbConnect.js
@@ -0,0 +1,20 @@
+// require('dotenv').config()
+// const MongoClient = require('mongodb').MongoClient;
+ 
+// //Database connection
+
+// const uri = 'mongodb+srv: //Akila:<Sooriyabandara123>@cluster0.rumcwol.mongodb.net/test'
+// const client =  new MongoClient(uri,{ useNewUrlParser: true })
+
+
+// client.connect((err,db) => {
+//             if(!err) {
+//                 console.log('MongoDB Connected')
+//             }
+//             else {
+//                 console.log("DB Error: ", err);
+//                 process.exit(1);
+//             }
+//         })
+    
+//     module.MongoClient = client;
